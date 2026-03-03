2bcb49f6e6a2179014a79a42faa1d6d5779a7b13
vallreey <revalsapertama@gmail.com>
2026-03-03 14:27:55 +0700

    Perbaikan Code dan Fix dari TP 02

--- 02_Pemrograman_JavaScript/TP/index.js
+++ 02_Pemrograman_JavaScript/TP/index.js
@@ -3,7 +3,7 @@ const arr1 = [2, 0, 26, 28, -2];
 function mulOfArray(arr) {
     let result = 1;
     for (let i = 0; i < arr.length; i = i + 1) {
-       if (arr[i] >= 0) {
+       if (arr[i] > 0) {
            result = result * arr[i];
        }
     }
